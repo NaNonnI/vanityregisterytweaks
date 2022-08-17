7e211fa8ea6e6becba495ec5e28b2add7a88e46f
Nonomi <42698703+NaNonnI@users.noreply.github.com>
2022-08-18 01:55:04 +0200

    Add files via upload

---
+++ VanityRegistryTweak/main.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(main))
+        Me.topbar = New System.Windows.Forms.Panel()
+        Me.logo = New System.Windows.Forms.PictureBox()
+        Me.minimizeb = New System.Windows.Forms.PictureBox()
+        Me.closeb = New System.Windows.Forms.PictureBox()
+        Me.secbuttons = New System.Windows.Forms.Button()
+        Me.seclabel = New System.Windows.Forms.Label()
+        Me.wvbutton = New System.Windows.Forms.Button()
+        Me.wvlabel = New System.Windows.Forms.Label()
+        Me.tobutton = New System.Windows.Forms.Button()
+        Me.tolabel = New System.Windows.Forms.Label()
+        Me.ebutton = New System.Windows.Forms.Button()
+        Me.elabel = New System.Windows.Forms.Label()
+        Me.topbar.SuspendLayout()
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.minimizeb, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.closeb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'topbar
+        '
+        Me.topbar.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.topbar.Controls.Add(Me.logo)
+        Me.topbar.Controls.Add(Me.minimizeb)
+        Me.topbar.Controls.Add(Me.closeb)
+        resources.ApplyResources(Me.topbar, "topbar")
+        Me.topbar.Name = "topbar"
+        '
+        'logo
+        '
+        Me.logo.BackColor = System.Drawing.Color.Transparent
+        Me.logo.Image = Global.VanityRegistryTweak.My.Resources.Resources.vrt
+        resources.ApplyResources(Me.logo, "logo")
+        Me.logo.Name = "logo"
+        Me.logo.TabStop = False
+        '
+        'minimizeb
+        '
+        Me.minimizeb.Image = Global.VanityRegistryTweak.My.Resources.Resources.minimize
+        resources.ApplyResources(Me.minimizeb, "minimizeb")
+        Me.minimizeb.Name = "minimizeb"
+        Me.minimizeb.TabStop = False
+        '
+        'closeb
+        '
+        Me.closeb.Image = Global.VanityRegistryTweak.My.Resources.Resources.close
+        resources.ApplyResources(Me.closeb, "closeb")
+        Me.closeb.Name = "closeb"
+        Me.closeb.TabStop = False
+        '
+        'secbuttons
+        '
+        resources.ApplyResources(Me.secbuttons, "secbuttons")
+        Me.secbuttons.Name = "secbuttons"
+        Me.secbuttons.UseVisualStyleBackColor = True
+        '
+        'seclabel
+        '
+        resources.ApplyResources(Me.seclabel, "seclabel")
+        Me.seclabel.Name = "seclabel"
+        '
+        'wvbutton
+        '
+        resources.ApplyResources(Me.wvbutton, "wvbutton")
+        Me.wvbutton.Name = "wvbutton"
+        Me.wvbutton.UseVisualStyleBackColor = True
+        '
+        'wvlabel
+        '
+        resources.ApplyResources(Me.wvlabel, "wvlabel")
+        Me.wvlabel.Name = "wvlabel"
+        '
+        'tobutton
+        '
+        resources.ApplyResources(Me.tobutton, "tobutton")
+        Me.tobutton.Name = "tobutton"
+        Me.tobutton.UseVisualStyleBackColor = True
+        '
+        'tolabel
+        '
+        resources.ApplyResources(Me.tolabel, "tolabel")
+        Me.tolabel.Name = "tolabel"
+        '
+        'ebutton
+        '
+        resources.ApplyResources(Me.ebutton, "ebutton")
+        Me.ebutton.Name = "ebutton"
+        Me.ebutton.UseVisualStyleBackColor = True
+        '
+        'elabel
+        '
+        resources.ApplyResources(Me.elabel, "elabel")
+        Me.elabel.Name = "elabel"
+        '
+        'main
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.elabel)
+        Me.Controls.Add(Me.ebutton)
+        Me.Controls.Add(Me.tolabel)
+        Me.Controls.Add(Me.tobutton)
+        Me.Controls.Add(Me.wvlabel)
+        Me.Controls.Add(Me.wvbutton)
+        Me.Controls.Add(Me.seclabel)
+        Me.Controls.Add(Me.secbuttons)
+        Me.Controls.Add(Me.topbar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "main"
+        Me.TransparencyKey = System.Drawing.Color.Transparent
+        Me.topbar.ResumeLayout(False)
+        CType(Me.logo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.minimizeb, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.closeb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents topbar As Panel
+    Friend WithEvents closeb As PictureBox
+    Friend WithEvents minimizeb As PictureBox
+    Friend WithEvents logo As PictureBox
+    Friend WithEvents secbuttons As Button
+    Friend WithEvents seclabel As Label
+    Friend WithEvents wvbutton As Button
+    Friend WithEvents wvlabel As Label
+    Friend WithEvents tobutton As Button
+    Friend WithEvents tolabel As Label
+    Friend WithEvents ebutton As Button
+    Friend WithEvents elabel As Label
+End Class
---
+++ VanityRegistryTweak/main.vb
@@ -0,0 +1,137 @@
+﻿Imports Microsoft.Win32
+Public Class main
+
+    Dim regKey As RegistryKey
+
+    Private Sub secbutton_Click(sender As Object, e As EventArgs) Handles secbuttons.Click
+        regKey = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", True)
+        If seclabel.Text = "Désactivé" Then
+            regKey.CreateSubKey("ShowSecondsInSystemClock")
+            regKey.SetValue("ShowSecondsInSystemClock", &H1, RegistryValueKind.DWord)
+        Else
+            regKey.DeleteValue("ShowSecondsInSystemClock")
+        End If
+        regKey.Close()
+        RestartExplorer()
+        ReloadKeys()
+    End Sub
+    Private Sub wvbutton_Click(sender As Object, e As EventArgs) Handles wvbutton.Click
+        regKey = Registry.CurrentUser.OpenSubKey("Control Panel\Desktop", True)
+        If wvlabel.Text = "Désactivé" Then
+            regKey.CreateSubKey("PaintDesktopVersion")
+            regKey.SetValue("PaintDesktopVersion", &H1, RegistryValueKind.DWord)
+        Else
+            regKey.DeleteValue("PaintDesktopVersion")
+        End If
+        regKey.Close()
+        RestartExplorer()
+        ReloadKeys()
+    End Sub
+
+    Private Sub tobutton_Click(sender As Object, e As EventArgs) Handles tobutton.Click
+        regKey = Registry.ClassesRoot.OpenSubKey("\*\shell", True)
+        If tolabel.Text = "Désactivé" Then
+
+            Dim command As String = "cmd.exe /c takeown /f \""%1\"" /r /d y && icacls \""%1\"" /grant administrators:F /t"
+
+            regKey.CreateSubKey("runas")
+            regKey = Registry.ClassesRoot.OpenSubKey("\*\shell\runas", True)
+            regKey.SetValue("", "Take Ownership", RegistryValueKind.String)
+            regKey.SetValue("NoWorkingDirectory", "", RegistryValueKind.String)
+            regKey.CreateSubKey("command")
+            regKey = Registry.ClassesRoot.OpenSubKey("\*\shell\runas\command", True)
+            regKey.SetValue("", command, RegistryValueKind.String)
+            regKey.SetValue("IsolatedCommand", command, RegistryValueKind.String)
+
+            regKey = Registry.ClassesRoot.OpenSubKey("\Directory\shell", True)
+            regKey.CreateSubKey("runas")
+            regKey = Registry.ClassesRoot.OpenSubKey("\*\shell\runas", True)
+            regKey.SetValue("", "Take Ownership", RegistryValueKind.String)
+            regKey.SetValue("NoWorkingDirectory", "", RegistryValueKind.String)
+            regKey.CreateSubKey("command")
+            regKey = Registry.ClassesRoot.OpenSubKey("\*\shell\runas\command", True)
+            regKey.SetValue("", command, RegistryValueKind.String)
+            regKey.SetValue("IsolatedCommand", command, RegistryValueKind.String)
+        Else
+            regKey.DeleteSubKeyTree("runas")
+            regKey = Registry.ClassesRoot.OpenSubKey("\Directory\shell", True)
+            regKey.DeleteSubKeyTree("runas")
+        End If
+        regKey.Close()
+        RestartExplorer()
+        ReloadKeys()
+    End Sub
+
+    Private Sub ebutton_Click(sender As Object, e As EventArgs) Handles ebutton.Click
+        regKey = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", True)
+        If elabel.Text = "Désactivé" Then
+            regKey.CreateSubKey("EncryptionContextMenu")
+            regKey.SetValue("EncryptionContextMenu", &H1, RegistryValueKind.DWord)
+        Else
+            regKey.DeleteValue("EncryptionContextMenu")
+        End If
+        regKey.Close()
+        RestartExplorer()
+        ReloadKeys()
+    End Sub
+
+    Dim mouse_offset
+    Private Sub topbar_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) _
+    Handles topbar.MouseDown
+        mouse_offset = New Point(-e.X, -e.Y)
+    End Sub
+
+    Private Sub topbar_MouseMove(ByVal sender As Object,
+    ByVal e As System.Windows.Forms.MouseEventArgs) _
+    Handles topbar.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Dim mousePos As Point = Control.MousePosition
+            mousePos.Offset(mouse_offset.X, mouse_offset.Y)
+            Location = mousePos
+        End If
+    End Sub
+
+    Private Sub closeb_Click(sender As Object, e As EventArgs) Handles closeb.Click
+        Me.Close()
+        Me.Dispose()
+    End Sub
+
+    Private Sub minimizeb_Click(sender As Object, e As EventArgs) Handles minimizeb.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub RestartExplorer()
+        For Each prog As Process In Process.GetProcesses
+            If prog.ProcessName = "explorer" Then
+                prog.Kill()
+            End If
+        Next
+    End Sub
+
+    Private Sub ReloadKeys()
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "ShowSecondsInSystemClock", Nothing) Is Nothing Then
+            seclabel.Text = "Désactivé"
+        Else
+            seclabel.Text = "Activé"
+        End If
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "PaintDesktopVersion", Nothing) Is Nothing Then
+            wvlabel.Text = "Désactivé"
+        Else
+            wvlabel.Text = "Activé"
+        End If
+        If My.Computer.Registry.GetValue("HKEY_CLASSES_ROOT\*\shell\runas", "NoWorkingDirectory", Nothing) Is Nothing Then
+            tolabel.Text = "Désactivé"
+        Else
+            tolabel.Text = "Activé"
+        End If
+        If My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", "EncryptionContextMenu", Nothing) Is Nothing Then
+            elabel.Text = "Désactivé"
+        Else
+            elabel.Text = "Activé"
+        End If
+    End Sub
+
+    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReloadKeys()
+    End Sub
+End Class
